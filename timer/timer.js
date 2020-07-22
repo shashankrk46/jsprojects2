df63b3e61847be09e4ba6baa26c71e7b2b8653e2
shashankrk46 <shashankrk46@gmail.com>
2020-07-22 21:39:36 +0530

    started using svg

--- timer/timer.js
+++ timer/timer.js
@@ -21,7 +21,7 @@ class Timer{
             this.onStart()
         }
         this.tick();
-       this.interval= setInterval(this.tick,1000);
+       this.interval= setInterval(this.tick,50);
     }
 
     pause=()=>{
@@ -37,7 +37,7 @@ class Timer{
                 this.onComplete();
                }
         }else{
-            this.timeRemaining=this.timeRemaining-1;
+            this.timeRemaining=this.timeRemaining-0.5;
             if(this.onTick){
                 this.onTick();
                }
@@ -50,7 +50,7 @@ class Timer{
        return parseFloat(this.durationInput.value );
     }
     set timeRemaining(time){
-        this.durationInput.value=time;
+        this.durationInput.value=time.toFixed(2);
     }
 
 }
