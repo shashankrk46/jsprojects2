f2744e67208547c6ca51460a1469c80c63569596
shashankrk46 <shashankrk46@gmail.com>
2020-07-26 11:07:32 +0530

    refactored

--- timer/app.js
+++ timer/app.js
@@ -2,6 +2,7 @@
 const durationInput=document.querySelector('#duration')
 const startBtn=document.querySelector('.start');
 const pauseBtn=document.querySelector('.pause');
+const resumeBtn=document.querySelector('.resume');
 const circle=document.querySelector('circle');
 
 const perimeter=circle.getAttribute('r')*2*Math.PI;
@@ -11,9 +12,13 @@ let duration;
 // function removeFill(){
 //     circle.setAttribute('fill','transparent')
 // }
-const timer=new Timer(durationInput,startBtn,pauseBtn,{
+const timer=new Timer(durationInput,startBtn,pauseBtn,resumeBtn,{
     onStart(totalDuration){
       duration=totalDuration;
+    },
+    onResumed(timeRemaining){
+      circle.setAttribute('stroke-dashoffset',
+      perimeter*timeRemaining/duration-perimeter);
     },
     onTick(timeRemaining){
         
@@ -22,8 +27,8 @@ const timer=new Timer(durationInput,startBtn,pauseBtn,{
         
     },
     onComplete(){
-        
-        circle.setAttribute('fill','#D4458E')
+        console.log("completed")
+        // circle.setAttribute('fill','#D4458E')
         // setTimeout(removeFill,1000)
     }
     
--- timer/timer.js
+++ timer/timer.js
@@ -1,29 +1,65 @@
 class Timer{
-    constructor(durationInput,startBtn,pauseBtn,callbacks){
+    constructor(durationInput,startBtn,pauseBtn,resumeBtn,callbacks){
         this.durationInput=durationInput;
         this.startBtn=startBtn;
         this.pauseBtn=pauseBtn;
+        this.resumeBtn=resumeBtn;
 
         if(callbacks){
             this.onStart=callbacks.onStart;
             this.onTick=callbacks.onTick;
             this.onComplete=callbacks.onComplete;
+            this.onResumed=callbacks.onResumed;
         }
         
         this.startBtn.addEventListener('click',this.start)
-        this.pauseBtn.addEventListener('click',this.pause)    
+        this.pauseBtn.addEventListener('click',this.pause)  
+        this.resumeBtn.addEventListener('click',this.resume)  
     }
-
+    
     start=()=>{
         if(this.onStart){
             this.onStart(this.timeRemaining)
+
         }
         this.tick();
-       this.interval= setInterval(this.tick,20);
+        
+        this.startBtn.disabled=true;
+       this.interval=setInterval(this.tick,20);
     }
 
     pause=()=>{
          clearInterval(this.interval)
+         clearInterval(this.resAt)
+         this.startBtn.disabled=true;
+         this.resumeBtn.disabled=false;
+        //  setInterval(this.timeRemaining=this.timeRemaining-.02,20)
+    }
+    resume=()=>{
+        this.resumeBtn.disabled=true;  
+    this.resAt=setInterval(this.res,20);
+        
+    }
+    res=()=>{
+        if(this.timeRemaining<=0){
+            this.pause();
+            
+            this.startBtn.disabled=true;
+            if(this.onComplete){
+                this.onComplete();
+               }
+               
+               location.reload();
+               
+           
+        }else{
+            
+            this.timeRemaining=this.timeRemaining-.02;
+            if(this.onResumed){
+                this.onResumed(this.timeRemaining);
+               }
+        }
+
     }
 
     tick=()=>{
@@ -35,8 +71,11 @@ class Timer{
             if(this.onComplete){
                 this.onComplete();
                }
-               location.reload();
+               
+            location.reload();
+           
         }else{
+            
             this.timeRemaining=this.timeRemaining-.02;
             if(this.onTick){
                 this.onTick(this.timeRemaining);
