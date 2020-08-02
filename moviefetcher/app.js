317a9525a26039fe9d4f22979db9fdff619ab3e0
shashankrk46 <shashankrk46@gmail.com>
2020-08-02 09:51:21 +0530

    fetched api data

--- moviefetcher/app.js
+++ moviefetcher/app.js
@@ -6,20 +6,37 @@ params:{
     s:searchTerm,
 }
 })
-    console.log(res.data)
+  
+if(res.data.Error){
+    return [];
+}
+
 };
 
 const input=document.querySelector('input');
 
-let timeoutId;
-const onInput=e=>{
-    if(timeoutId){
-        clearTimeout(timeoutId)
-    }
-    timeoutId=setTimeout(()=>{
-        fetchData(e.target.value)  
-    },1000)
-        
+
+
+const onInput=async e=>{
+   const movies=await fetchData(e.target.value) ;
+   
+  movies.forEach(movie=>{
+
+    const div=document.createElement('div');
+    div.innerHTML=`
+    <img src="${movie.Poster}"/>
+    <h1>${movie.Title}</h1>
+    `
+    document.querySelector('#target').appendChild(div)
+    
+  })
 };
-input.addEventListener('input',onInput) ;    
+
+input.addEventListener('input',debounce(onInput)) ;   
+
+
+ 
+
+
+
    
---
+++ moviefetcher/utils.js
@@ -0,0 +1,14 @@
+const debounce=(func)=>{
+    let timeoutId;
+   
+    return (...args)=>{
+        if(timeoutId){
+             clearTimeout(timeoutId)
+        }
+        timeoutId=setTimeout(()=>{
+            
+            func.apply(null,args);
+        },1000)
+
+    }
+}
