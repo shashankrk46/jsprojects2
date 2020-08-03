01eb980f59aa6e93d468346f21c4ec0d23cf0c9c
shashankrk46 <shashankrk46@gmail.com>
2020-08-03 19:59:00 +0530

    fixed poster

--- moviefetcher/app.js
+++ moviefetcher/app.js
@@ -10,29 +10,51 @@ params:{
 if(res.data.Error){
     return [];
 }
-
+return res.data.Search;
 };
 
-const input=document.querySelector('input');
 
+const root=document.querySelector('.autocomplete');
+root.innerHTML=`
+<label><b>Search for a movie</b></label>
+<input class="input"/>
+<div class="dropdown">
+<div class="dropdown-menu">
+<div class="dropdown-content results"></div>
+</div>
+</div>
+
+`;
+
+const input=document.querySelector('input');
+const dropdown=document.querySelector('.dropdown');
+const resultsWrapper=document.querySelector('.results');
 
 
 const onInput=async e=>{
    const movies=await fetchData(e.target.value) ;
-   
-  movies.forEach(movie=>{
 
-    const div=document.createElement('div');
-    div.innerHTML=`
-    <img src="${movie.Poster}"/>
-    <h1>${movie.Title}</h1>
+   resultsWrapper.innerHTML='';
+   dropdown.classList.add('is-active')
+  for(let movie of movies){
+
+    const option=document.createElement('a');
+    const imgSrc=movie.Poster==='N/A'?'':movie.Poster
+    option.classList.add('dropdown-item')
+    option.innerHTML=`
+    <img src="${imgSrc}"/>
+    ${movie.Title}
     `
-    document.querySelector('#target').appendChild(div)
+   resultsWrapper.appendChild(option)
     
-  })
+  }
 };
 
-input.addEventListener('input',debounce(onInput)) ;   
+input.addEventListener('input',debounce(onInput)) ; 
+
+
+
+
 
 
  
