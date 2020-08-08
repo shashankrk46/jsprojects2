113864dd2014ce05020ab02a3aac1db6dab2e3b0
shashankrk46 <shashankrk46@gmail.com>
2020-08-08 11:38:16 +0530

    compared both sides but need to fix few errors

--- moviefetcher/app.js
+++ moviefetcher/app.js
@@ -30,7 +30,7 @@ createAutoComplete({
    root:document.querySelector('#left-autocomplete'),
    onOptionSelect(movie){
     document.querySelector('.tutorial').classList.add('is-hidden')
-    onMovieSelect(movie,document.querySelector('#left-summary'))
+    onMovieSelect(movie,document.querySelector('#left-summary'),'left')
 },
    
 });
@@ -39,25 +39,76 @@ createAutoComplete({
    root:document.querySelector('#right-autocomplete'),
    onOptionSelect(movie){
     document.querySelector('.tutorial').classList.add('is-hidden')
-    onMovieSelect(movie,document.querySelector('#right-summary'))
+    onMovieSelect(movie,document.querySelector('#right-summary'),'right')
 },
    
 });
 
-
-const onMovieSelect=async(movie,summaryElement)=>{
+let leftMovie;
+let rightMovie;
+const onMovieSelect=async(movie,summaryElement,side)=>{
     const res=await axios.get('http://www.omdbapi.com/',
     {
     params:{
         apikey:'a3cb309a',
         i:movie.imdbID,
-    }
-})
-summaryElement.innerHTML=movieTemplate(res.data)
 
+    }
+});
+summaryElement.innerHTML=movieTemplate(res.data);
+
+if(side==='left'){
+    leftMovie=res.data
+}else{
+    rightMovie=res.data
+}
+
+if(leftMovie&&rightMovie){
+    runComparison();
+}
+}
+
+const runComparison=()=>{
+    const leftSideStats=document.querySelectorAll('#left-summary .notification');
+    const rightSideStats=document.querySelectorAll('#right-summary .notification');
+    leftSideStats.forEach((leftStats,index)=>{
+        const rightStats=rightSideStats[index];
+
+        const leftSidevalue=leftStats.dataset.value;
+        const rightSidevalue=rightStats.dataset.value;
+
+        console.log(leftSidevalue);
+        console.log(rightSidevalue);
+
+        if(rightSidevalue>leftSidevalue){
+            leftStats.classList.remove('is-primary');
+            leftStats.classList.add('is-warning');
+        }else{
+            rightStats.classList.remove('is-primary');
+            rightStats.classList.add('is-warning');
+        }
+    })
 }
 
 const movieTemplate=(movieDetail)=>{
+
+
+    const dollars=+movieDetail.BoxOffice.replace(/\$/g,'').replace(/,/g,'');
+    const metaScore=+movieDetail.Metascore;
+    const imdbScore=parseFloat(movieDetail.imdbRating);
+    const imdbVotes=+movieDetail.imdbVotes.replace(/,/g,'');
+    
+    const awards=movieDetail.Awards.split('').reduce((acc,word)=>{
+        const value=+(word);
+        if(isNaN(value)){
+            return acc
+        }else{
+            return acc+value;
+        }
+    },0);
+ 
+
+
     return `<article class="media">
     <figure class="media-left">
     <p class="image">
@@ -72,24 +123,24 @@ const movieTemplate=(movieDetail)=>{
     </div>
     </div>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${awards} class="notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
     </article>
     
-    <article class="notification is-primary">
+    <article data-value=${dollars} class="notification is-primary">
     <p class="title">${movieDetail.BoxOffice}</p>
     <p class="subtitle">Box office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${metaScore} class="notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbScore} class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbVotes}  class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
     </article>
